97e814738ae6ca7cd75e81f4176fbfdd78a9dc45
nizar333 <nizar333.na@gmail.com>
2017-03-13 14:37:22 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
-var url = process.env.MONGODB_URI || process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/test';
+var url = 'mongodb://127.0.0.1:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
